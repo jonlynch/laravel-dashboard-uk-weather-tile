192787cdbad4c8f073f71c29a040d753d193ec43
Jon Lynch <jon.lynch@ymail.com>
2020-06-11 22:35:03 +0100

    Add in wind direction

--- src/Services/Forecasts.php
+++ src/Services/Forecasts.php
@@ -19,6 +19,8 @@ class Forecasts
         $response = Http::withHeaders($headers)
             ->get($endpoint)
             ->json();
+
+        
         
         return collect($response['features'][0]['properties']['timeSeries'])
             ->map(function (array $forecast) {
@@ -27,10 +29,11 @@ class Forecasts
                     'temp' => $forecast['screenTemperature'],
                     'windSpeed' => $forecast['windSpeed10m'],
                     'windGust' => $forecast['max10mWindGust'] ?? '',
-                    'windDir' => $forecast['windDirectionFrom10m'],
+                    'windBearing' => $forecast['windDirectionFrom10m'],
                     'feelsLike' => $forecast['feelsLikeTemperature'],
                     'probOfPrecip' => $forecast['probOfPrecipitation'],
                     'precipRate' => $forecast['precipitationRate'],
+                    'windDir' => self::windRose($forecast['windDirectionFrom10m'])
                 ];
             })
             ->filter(function ($forecast){
@@ -40,4 +43,26 @@ class Forecasts
             ->take(24)
             ->toArray();
     }
+
+    public static function windRose($bearing) {
+        $winddir[]="N";
+        $winddir[]="NNE";
+        $winddir[]="NE";
+        $winddir[]="ENE";
+        $winddir[]="E";
+        $winddir[]="ESE";
+        $winddir[]="SE";
+        $winddir[]="SSE";
+        $winddir[]="S";
+        $winddir[]="SSW";
+        $winddir[]="SW";
+        $winddir[]="WSW";
+        $winddir[]="W";
+        $winddir[]="WNW";
+        $winddir[]="NW";
+        $winddir[]="NNW";
+        $winddir[]="N";
+        return $winddir[round($bearing*16/360)];
+   }
+   
 }
